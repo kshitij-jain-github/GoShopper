9d3e03040eb146546aad1554d436406a98c1cfff
Kshitij Jain <jainkshitij1530@gmail.com>
2023-11-18 12:09:02 +0530

    Add project files.

---
+++ GoShopper/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GoShopper.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
