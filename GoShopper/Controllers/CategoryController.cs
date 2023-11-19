f363a884709247021730b7ce283098adf406cbfd
Kshitij Jain <jainkshitij1530@gmail.com>
2023-11-19 11:49:37 +0530

    Add Caterogy Controller and view

---
+++ GoShopper/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using GoShopper.Data;
+using GoShopper.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoShopper.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _db; 
+        public CategoryController(AppDbContext db) {
+            _db = db;
+        } 
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();   
+            return View();
+        }
+    }
+}
