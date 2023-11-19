1ddf7ffde847bdaa00646eaacd76f44c125be010
Kshitij Jain <jainkshitij1530@gmail.com>
2023-11-19 11:46:23 +0530

    db Categories

--- GoShopper/Data/AppDbContext.cs
+++ GoShopper/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using GoShopper.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace GoShopper.Data
 {
@@ -7,5 +8,6 @@ namespace GoShopper.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
+        public DbSet<Category> Categories { get; set; }
     }
 }
