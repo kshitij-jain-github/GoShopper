ce938a0dd35fb65a02edcccf543794c2ddc0de67
Kshitij Jain <jainkshitij1530@gmail.com>
2023-11-23 13:40:03 +0530

    Add generic-repository

---
+++ GoShopper.DataAccess/Repository/Repository.cs
@@ -0,0 +1,51 @@
+﻿using GoShopper.Data;
+using GoShopper.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoShopper.DataAccess.Repository
+{
+    internal class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly AppDbContext _db;
+        internal DbSet<T> dbSet;
+        public Repository(AppDbContext db)
+        {
+            _db=db;
+            this.dbSet=_db.Set<T>();
+        }
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = false)
+        {
+            IQueryable<T> query =  dbSet;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
+            
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null)
+        {
+            IQueryable<T> query = dbSet;
+             return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
