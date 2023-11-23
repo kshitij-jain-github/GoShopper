5a518be56b288534ed3692b915be0f61aae81665
Kshitij Jain <jainkshitij1530@gmail.com>
2023-11-23 18:50:07 +0530

    Add caterogy-repository

---
+++ GoShopper.DataAccess/Repository/CategoryRepositroy.cs
@@ -0,0 +1,25 @@
+﻿using GoShopper.Data;
+using GoShopper.DataAccess.Repository.IRepository;
+using GoShopper.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoShopper.DataAccess.Repository
+{
+    internal class CategoryRepositroy : Repository<Category>, ICategoryRepository
+    {
+        private AppDbContext _db;
+        public CategoryRepositroy(AppDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Category obj)
+        {
+           _db.Categories.Update(obj);  
+        }
+    }
+}
