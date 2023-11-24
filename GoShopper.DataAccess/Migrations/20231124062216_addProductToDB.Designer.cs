44ad7c216c138ff99c0a8ca0973d88882a136482
Kshitij Jain <jainkshitij1530@gmail.com>
2023-11-24 12:15:39 +0530

    Integrated Category-with-Product-FK

--- GoShopper.DataAccess/Migrations/20231124062216_addProductToDB.Designer.cs
+++ GoShopper.DataAccess/Migrations/20231124062216_addProductToDB.Designer.cs
@@ -3,6 +3,7 @@ using GoShopper.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GoShopper.DataAccess.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231124062216_addProductToDB")]
+    partial class addProductToDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- GoShopper/Areas/Admin/Controllers/ProductController.cs
+++ GoShopper/Areas/Admin/Controllers/ProductController.cs
@@ -2,6 +2,7 @@
 using GoShopper.DataAccess.Repository.IRepository;
 using GoShopper.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace GoShopper.Areas.Admin.Controllers
 {
@@ -16,11 +17,20 @@ namespace GoShopper.Areas.Admin.Controllers
         public IActionResult Index()
         {
             List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
+        
+        
             return View(objProductList);
         }
 
         public IActionResult Create()
         {
+            IEnumerable<SelectListItem> categoryList = _unitOfWork.Category
+            .GetAll().ToList().Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id.ToString()
+            });
+            ViewBag.Category = categoryList;    
             return View();
         }
         [HttpPost]
@@ -43,6 +53,7 @@ namespace GoShopper.Areas.Admin.Controllers
                 return NotFound();
             }
             Product? ProductFromDb = _unitOfWork.Product.Get(u => u.Id == id);
+          
             //Product? ProductFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id==id);
             //Product? ProductFromDb2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
 
