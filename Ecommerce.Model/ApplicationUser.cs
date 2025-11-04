52fdd60440a6e6290fe9c6d74bd0eff399250a3d
KSHITIJ JAIN <134933216+kshitij-jain-github@users.noreply.github.com>
2025-11-04 10:30:50 +0530

    chagne

--- Ecommerce.Model/ApplicationUser.cs
+++ Ecommerce.Model/ApplicationUser.cs
@@ -13,7 +13,7 @@ namespace Ecommerce.Model
     public class ApplicationUser : IdentityUser
     {
 		[Required]
-		public string Name { get; set; }
+		public string Nae { get; set; }
 		public string? StreetAddress { get; set; }
 		public string? City { get; set; }
 		public string? State { get; set; }
